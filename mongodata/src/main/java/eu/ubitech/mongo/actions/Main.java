881be2d297341e9d9cec51b58dd4c0baa4918850
George Vafiadis <vafeiadis.giorgos@gmail.com>
2017-05-31 17:09:04 +0300

    Add process of finding the max value of each key.

--- mongodata/src/main/java/eu/ubitech/mongo/actions/Main.java
+++ mongodata/src/main/java/eu/ubitech/mongo/actions/Main.java
@@ -6,16 +6,25 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import scala.Tuple2;
 
 @SpringBootApplication
 public class Main implements CommandLineRunner {
 
     @Autowired
     private PersonRepository repository;
+    
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
@@ -24,6 +33,7 @@ public class Main implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
+        //put values in MongoDB
         HashMap<Integer, String> hmap = new HashMap<>();
         hmap.put(12, "Anthony");
         hmap.put(18, "George");
@@ -38,6 +48,26 @@ public class Main implements CommandLineRunner {
             persons.add(new Person((int) mentry.getKey(), (String) mentry.getValue()));
             repository.save(persons);
         }
-    }
 
+        //Find max values for each key
+        List<Person> data = repository.findAll();
+
+        long startTime = System.currentTimeMillis();
+        SparkConf conf = new SparkConf().setMaster("local").setAppName("SparkAndMongo");
+        JavaSparkContext context = new JavaSparkContext(conf);
+        JavaRDD<Person> rdd = context.parallelize(data);
+
+        JavaPairRDD<String, Integer> keyValuePairs = rdd.mapToPair(obj -> {
+            Person person = obj;
+            return new Tuple2(person.getName(), person.getAge());
+        });
+
+        JavaPairRDD<String, Integer> result = keyValuePairs.reduceByKey((v1, v2) -> Math.max(v1, v2));
+
+        result.collect().stream().forEach((element) -> {
+            LOGGER.log(Level.INFO, "Logging an INFO-level message: {0}", "Max result: (" + element._1 + ", " + element._2 + ")");
+        });
+        long endTime = System.currentTimeMillis();
+        LOGGER.log(Level.INFO, "Logging an INFO-level message: {0}", "Max values found. Time :" + (endTime - startTime));
+    }
 }
